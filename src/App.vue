98cea00fd8e86a65024edc36017463885a89fa6e
wandukongkong <toutopia0@gmail.com>
2023-08-18 00:04:55 +0900

    vue로 전환

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script >
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width:100%;
+  height:100%;
+}
+
+html, body{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
--- src/components/Menu/Menu.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {Box, List, ListItem, ListItemButton} from '@mui/material'
-
-
-
-export default Menu
-
