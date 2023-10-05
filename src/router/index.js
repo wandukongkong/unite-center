479aa556e6fba0fb2b7bfa5eb64f1d6270f71855
WKK <toutopia0@gmail.com>
2023-10-05 23:52:29 +0900

    createWebHashHistory 추가 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import WelcomeView from '../views/WelcomeView.vue';
-import LoginView from '../views/LoginView.vue';
-import CheckView from '../views/CheckView.vue';
-import RandomPickView from '../views/RandomPickView.vue';
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import WelcomeView from "../views/WelcomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import CheckView from "../views/CheckView.vue";
+import RandomPickView from "../views/RandomPickView.vue";
 
 const routes = [
-	{
-		path: '/',
-		component: WelcomeView,
-	},
-	{
-		path: '/login',
-		component: LoginView,
-	},
-	{
-		path: '/check',
-		component: CheckView,
-	},
-	{
-		path: '/randomPick',
-		component: RandomPickView,
-	},
+  {
+    path: "/",
+    component: WelcomeView,
+  },
+  {
+    path: "/login",
+    component: LoginView,
+  },
+  {
+    path: "/check",
+    component: CheckView,
+  },
+  {
+    path: "/randomPick",
+    component: RandomPickView,
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
