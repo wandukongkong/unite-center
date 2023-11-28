f673269d08da379be8e0b42f7faf1a96f7c6ffa0
WKK <toutopia0@gmail.com>
2023-11-29 00:10:33 +0900

    메뉴 구성 완료

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+	<Header></Header>
 	<router-view />
 </template>
 
@@ -47,15 +48,43 @@ body {
 	opacity: 0;
 }
 
-.v-enter-active,
-.v-leave-active {
-	transition: opacity 0.5s ease;
+.slide-menu-fade-enter-active {
+	transition: all 0.3s ease-out;
 }
 
-.v-enter-from,
-.v-leave-to {
+.slide-menu-fade-leave-active {
+	transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-menu-fade-enter-from,
+.slide-menu-fade-leave-to {
+	transform: translateX(-20px);
 	opacity: 0;
 }
+
+.fade-enter-active {
+	transition: all 0.3s ease-out;
+}
+
+.fade-leave-active {
+	transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+
+
+/* .fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.2s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+} */
 
 @media screen and (max-width: 576px) {
 	#app {
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { toRef } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+const isOpenMenu = toRef(true);
+
+</script>
+<template>
+  <div class="position-absolute w-100 start-0 top-0 ">
+    <div class="d-flex bg-green">
+      <button class="btn border-0 w-100px" @click="isOpenMenu = !isOpenMenu">
+        <i class="bi bi-list"></i>
+      </button>
+    </div>
+  </div>
+  <Transition name="slide-menu-fade">
+    <div v-if="isOpenMenu" class="position-absolute top-0 start-0 side-bar">
+      <div class="d-flex justify-content-end mb-5">
+        <button class="btn border-0 shadow-none" @click="isOpenMenu = !isOpenMenu">x</button>
+      </div>
+      <div class="d-flex flex-column">
+        <button class="btn border-0 shadow-none d-flex align-items-center mb-2 py-0" @click="() => {
+          router.push('/')
+          isOpenMenu = false
+        }">
+          <img src="../assets/img/pokemon/homeIcon.png" width="30" />
+          <span class="mx-2 mt-1 menu-font">Home</span>
+        </button>
+        <button class="btn border-0 shadow-none d-flex align-items-center mb-2 py-0" @click="() => {
+          router.push('/randomPick');
+          isOpenMenu = false
+        }">
+          <img src="../assets/img/pokemon/battleIcon.png" width="30" />
+          <span class="mx-2 mt-1 menu-font">Random Pick</span>
+        </button>
+      </div>
+    </div>
+  </Transition>
+  <Transition name="fade">
+    <div v-if="isOpenMenu" class="side-bar-back position-absolute top-0 start-0"></div>
+  </Transition>
+</template>
+<style>
+.menu-font {
+  font-size: 20pt;
+}
+
+.side-bar {
+  width: 250px;
+  height: 100%;
+  z-index: 999;
+  background-color: #fcb468;
+  box-shadow: 1px 1px 16px 0px #00000042;
+}
+
+.side-bar-back {
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  z-index: 998;
+  opacity: 0.3;
+}
+</style>
